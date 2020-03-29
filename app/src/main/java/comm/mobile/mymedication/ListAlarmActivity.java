239cff4d16b52c992a7d604c35adbb5f57b8bc36
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2020-03-29 22:07:52 +0700

    Add - ListAlarmActivity Add - Floating Button at ListAlarmActivity

---
+++ app/src/main/java/comm/mobile/mymedication/ListAlarmActivity.java
@@ -0,0 +1,14 @@
+package comm.mobile.mymedication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ListAlarmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_alarm);
+    }
+}
