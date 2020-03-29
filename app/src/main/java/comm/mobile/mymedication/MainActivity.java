27244174a22a5199b2f40e7dc024a64ca7240cc0
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2020-03-29 22:34:20 +0700

    Add - Button SetAlarm at MainActivity

--- app/src/main/java/comm/mobile/mymedication/MainActivity.java
+++ app/src/main/java/comm/mobile/mymedication/MainActivity.java
@@ -1,14 +1,28 @@
 package comm.mobile.mymedication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnSetAlarm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSetAlarm = findViewById(R.id.btn_SetAlarm);
+
+        btnSetAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent SetAlarm = new Intent(getApplicationContext(), ListAlarmActivity.class);
+                startActivity(SetAlarm);
+            }
+        });
     }
 }
